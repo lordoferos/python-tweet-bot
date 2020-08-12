26fe25074b7058fc49fc7f362f046d884af2e66f
lordoferos <33201932+lordoferos@users.noreply.github.com>
2020-08-12 08:56:46 +0300

    use your own login details

--- graphie.py
+++ graphie.py
@@ -1,8 +1,8 @@
 import tweepy
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("d4Jl6eH8AiYcXkuAwrgkM6gJ3", "Z5bhyKoDiOiKU9Uu9zPoxnKdAKShONdgsTkcKXeJLhuaJtqkSJ")
-auth.set_access_token("892981240667807744-rfF45SbCK9Yu0byTPoVLxQTyEDMvg2g", "Mx67izXSpNVkhc4wHvwEAn90dknkHoq7dv0W9RRTf28di")
+auth = tweepy.OAuthHandler("yyyyyyyyyyyyyyyyyyyyyyyyyy", "fffffffffffffffffffffffffffffff")
+auth.set_access_token("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm", "jcmktegdhdddddddddddddddddddd")
 
 # Create API Object
 api = tweepy.API(auth)
